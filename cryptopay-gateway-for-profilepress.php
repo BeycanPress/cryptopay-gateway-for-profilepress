9e7d8af2e7dd5014b2346a0af20bc3130d1137c8
Halil Beycan <50718965+BeycanDeveloper@users.noreply.github.com>
2024-04-04 16:45:55 +0800

    updated for wp.org

--- cryptopay-gateway-for-profilepress.php
+++ cryptopay-gateway-for-profilepress.php
@@ -10,7 +10,7 @@ defined('ABSPATH') || exit;
 // @phpcs:disable Generic.Files.LineLength
 
 /**
- * Plugin Name: ProfilePress - CryptoPay Gateway
+ * Plugin Name: CryptoPay Gateway for ProfilePress
  * Version:     1.0.0
  * Plugin URI:  https://beycanpress.com/cryptopay/
  * Description: Adds Cryptocurrency payment gateway (CryptoPay) for ProfilePress.
@@ -21,7 +21,7 @@ defined('ABSPATH') || exit;
  * Text Domain: pp-cryptopay
  * Tags: Cryptopay, Cryptocurrency, WooCommerce, WordPress, MetaMask, Trust, Binance, Wallet, Ethereum, Bitcoin, Binance smart chain, Payment, Plugin, Gateway, Moralis, Converter, API, coin market cap, CMC
  * Requires at least: 5.0
- * Tested up to: 6.4.3
+ * Tested up to: 6.5.0
  * Requires PHP: 8.1
 */
 
